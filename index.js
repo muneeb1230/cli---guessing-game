66fa4de0de4a12f162219ec34d9398754b40e15b
Muneeb Malik <144794302+muneeb1230@users.noreply.github.com>
2024-04-23 03:06:53 +0500

    repeat

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 // 1) computer will generate a random number.
-// 2) user input for guessing number. 
+// 2) user input for guessing number.
 // 3) compare user input with computer generated number and show result.
-const randomNumber = Math.round((Math.random)() * 6 + 1);
+const randomNumber = Math.round(Math.random() * 6 + 1);
 const answers = await inquirer.prompt([
     {
         name: "userGuessNumber",
         type: "number",
-        message: "please guess a number betwwen 1 to 6"
-    }
+        message: "please guess a number betwwen 1 to 6",
+    },
 ]);
 if (answers.userGuessNumber === randomNumber) {
-    console.log("congratutation! you guessed right number " + answers.userGuessNumber + " the random number was " + randomNumber);
+    console.log("congratutation! you guessed right number " +
+        answers.userGuessNumber +
+        " the random number was " +
+        randomNumber);
 }
 else {
-    console.log("sorry you guseed wrong number " + answers.userGuessNumber + " the random number was " + randomNumber);
+    console.log("sorry you guseed wrong number " +
+        answers.userGuessNumber +
+        " the random number was " +
+        randomNumber);
 }
--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+#! /usr/bin/env node
+
+
 
 import inquirer from "inquirer";
 // 1) computer will generate a random number.
